26d3c44f4cebb76950508983e49178f815691f82
koruri29 <koruri821@yahoo.co.jp>
2023-01-02 13:05:35 +0900

    問題を1回しかクリックできないように変更。 また、ボタンを押すと同時に、問題文の下に正解or不正解が表示されるように変更。

--- index.php
+++ index.php
@@ -46,6 +46,16 @@ $questions_str2 = implode(', ', $questions[2]);
 <body>
 	<main>
 		<h1>三択クイズ</h1>
+		<section class="dummy">
+			<h2>ダミー問題文</h2>
+			<ul>
+				<li class="notseikai">選択肢１</li>
+				<li class="seikai">選択肢２</li>
+				<li class="notseikai">選択肢３</li>
+			</ul>
+			<div class="answer-correct selected">正解です！</div>
+			<div class="answer-wrong">残念！不正解…</div>
+		</section>
 	</main>
 	<script>
 		'use strict';
@@ -58,39 +68,60 @@ $questions_str2 = implode(', ', $questions[2]);
 				const li0 = document.createElement('li');
 				const li1 = document.createElement('li');
 				const li2 = document.createElement('li');
+				const divAnswer = document.createElement('div');
+
+				function createAnswer(answer) {
+					if (answer) {
+						divAnswer.textContent = "正解です！";
+						divAnswer.className = 'answer-correct';
+					} else {
+						divAnswer.textContent = "残念！不正解…";
+						divAnswer.className = 'answer-wrong';
+					}
+					return divAnswer;
+				}
 				
 				h2.textContent = quiz[0];
 				li0.textContent = quiz[1];
 				li1.textContent = quiz[2];
 				li2.textContent = quiz[3];
 		
-				li0.addEventListener('click', () => {
-					if (quiz[4] == 0) {
-						li0.classList.add('correct');
-					} else {
-						li0.classList.add('wrong');
-					}
-				});
-				li1.addEventListener('click', () => {
-					if (quiz[4] == 1) {
-						li1.classList.add('correct');
-					} else {
-						li1.classList.add('wrong');
-					}
-				});
-				li2.addEventListener('click', () => {
-					if (quiz[4] == 2) {
-						li2.classList.add('correct');
-					} else {
-						li2.classList.add('wrong');
-					}
-				});
+				if (quiz[4] == 0) {
+					ul.addEventListener('click', e => {
+						if (e.target == li0) {
+							createAnswer(true);
+						} else {
+							createAnswer(false);
+						}
+					}, {once: true});
+				}
+
+				if (quiz[4] == 1) {
+					ul.addEventListener('click', e => {
+						if (e.target == li1) {
+							createAnswer(true);
+						} else {
+							createAnswer(false);
+						}
+					}, {once: true});
+				}
+
+				if (quiz[4] == 2) {
+					ul.addEventListener('click', e => {
+						if (e.target == li2) {
+							createAnswer(true);
+						} else {
+							createAnswer(false);
+						}
+					}, {once: true});
+				}
 		
 				ul.appendChild(li0);
 				ul.appendChild(li1);
 				ul.appendChild(li2);
 				section.appendChild(h2);
 				section.appendChild(ul);
+				section.appendChild(divAnswer);
 				main.appendChild(section);
 			}
 		
